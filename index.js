c0abe414c939edd23b022ce9f08022906a757da4
Brennan Newton <brennanmnewton@gmail.com>
2019-08-19 13:36:35 -0700

    Search style & js

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 import SNIPS from './snippet-data.js';
-// default import
-
-// 1. Map over snips
-// 2. Transform snip into HTML
-// 3. Put HTML into snips
-
-const renderSnips = snippets => {
-  const snippetHTML = snippets
-    .map(
-      snippet => /* html */ `
-    <div class="snip">
-      <div class="text">
-        <h2>${snippet.title}</h2>
-        <p>${snippet.description}</p>
-      </div>
-      <pre><code class="${snippet.language}">${he.encode(
-        snippet.code
-      )}</code></pre>
-    </div>`
-    )
-    .join('');
-
-  const snippetsElem = document.getElementById('snippets');
-  snippetsElem.innerHTML = snippetHTML;
-};
+import { renderSnips } from './lib/snippets.js';
+import search from './lib/search.js';
 
 renderSnips(SNIPS);
 
-hljs.initHighlightingOnLoad();
+document.getElementById('search-bar').addEventListener('submit', search);
